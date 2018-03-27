02f314c269ba05e93ffa44e0ecdd5375fbdf62ec
Inc-ho <878014750@qq.com>
2018-03-27 15:22:32 +0800

    测试提交git

---
+++ sell/src/main/java/com/imooc/GitTest/Test.java
@@ -0,0 +1,13 @@
+package com.imooc.GitTest;
+
+/**
+ * Create By Min
+ * 2018/3/27
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("测试提交！");
+    }
+
+}
