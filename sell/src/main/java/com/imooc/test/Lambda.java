05fc8d7ec67d5ac26723b69cd42d78af355cf4aa
Inc-ho <878014750@qq.com>
2018-03-27 15:16:30 +0800

    springboot微信点餐

---
+++ sell/src/main/java/com/imooc/test/Lambda.java
@@ -0,0 +1,14 @@
+package com.imooc.test;
+
+/**
+ * @USER Min
+ * @DATE 2018/3/15
+ */
+public class Lambda {
+
+    public static void main(String[] args) {
+        MyLambdaInterface a = (s)->System.out.println(s);
+    }
+
+
+}
---
+++ sell/src/main/java/com/imooc/test/MyLambdaInterface.java
@@ -0,0 +1,10 @@
+package com.imooc.test;
+
+/**
+ * @USER Min
+ * @DATE 2018/3/15
+ */
+@FunctionalInterface
+public interface MyLambdaInterface {
+    void dosometing(String s);
+}
